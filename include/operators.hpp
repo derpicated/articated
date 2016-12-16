3156b532c1820dc8f3444c742267046230f9f24f
Ingmar Delsink <ingmardelsink@hotmail.com>
2016-12-16 21:31:16 +0100

    add classification stubs

--- include/operators.hpp
+++ include/operators.hpp
@@ -1,11 +1,80 @@
 #ifndef OPERATORS_HPP
 #define OPERATORS_HPP
 
+#include <map>
+#include <sstream>
+
+typedef struct keypoint_t {
+    unsigned int x;
+    unsigned int y;
+} keypoint_t;
+
 class operators {
     private:
+    std::map<unsigned int, keypoint_t> _reference_markers = {};
+    const unsigned int _minimal_ref_points = 3;
+
     public:
     operators ();
-    virtual ~operators ();
+    ~operators ();
+
+
+    /**
+     * Quaternion
+     * Z [↻ Yaw ψ]  X [⟲ Roll φ]
+     * ^           ^
+     * |         /
+     * |       /
+     * |     /
+     * |   /
+     * | /
+     * +--------> Y [⟲ Pitch θ]
+     */
+
+    /**
+     * classify the scale from a number of keypoints to the reference
+     * Scale (z)
+     * @param  marker_points [description]
+     * @return               [description]
+     */
+    float classify_scale (const std::map<unsigned int, keypoint_t>& marker_points);
+
+    /**
+     * classify the translation from a number of keypoints to the reference
+     * Translation (x, y)
+     * @param  marker_points [description]
+     * @return               [description]
+     */
+    float classify_translation (const std::map<unsigned int, keypoint_t>& marker_points);
+
+    /**
+     * classify the rotation from a number of keypoints to the reference
+     * Rotation (yaw)
+     * @param  marker_points [description]
+     * @return               [description]
+     */
+    float classify_yaw (const std::map<unsigned int, keypoint_t>& marker_points);
+
+    // angle pitch
+    float classify_pitch (const std::map<unsigned int, keypoint_t>& marker_points);
+
+    // angle roll
+    float classify_roll (const std::map<unsigned int, keypoint_t>& marker_points);
+
+    /**
+     * set the reference from key points
+     * @param marker_points are the reference marker_points
+     * @return returns the reference marker_points
+     */
+    std::map<unsigned int, keypoint_t> set_reference (
+    const std::map<unsigned int, keypoint_t>& marker_points);
+
+    /**
+     * convert value to an std::string
+     * @param value to be converted
+     * @return returns a std::string
+     */
+    template <typename T> std::string to_string (T value);
 };
 
 #endif // OPERATORS_HPP
--- src/operators.cpp
+++ src/operators.cpp
@@ -1,7 +1,48 @@
 #include "operators.hpp"
+#include <iostream>
+#include <sstream>
+#include <string>
+
 
 operators::operators () {
 }
 
 operators::~operators () {
+}
+
+float classify_scale (const std::map<unsigned int, keypoint_t>& marker_points) {
+    return 0;
+}
+
+float classify_translation (const std::map<unsigned int, keypoint_t>& marker_points) {
+    return 0;
+}
+
+float classify_yaw (const std::map<unsigned int, keypoint_t>& marker_points) {
+    return 0;
+}
+
+float classify_pitch (const std::map<unsigned int, keypoint_t>& marker_points) {
+    return 0;
+}
+
+float classify_roll (const std::map<unsigned int, keypoint_t>& marker_points) {
+    return 0;
+}
+
+
+std::map<unsigned int, keypoint_t> operators::set_reference (
+const std::map<unsigned int, keypoint_t>& marker_points) {
+    if (marker_points.size () < _minimal_ref_points) {
+        throw std::length_error ("too few reference points found " +
+        to_string (marker_points.size ()) + " need " + to_string (_minimal_ref_points));
+    }
+    _reference_markers = marker_points;
+    return _reference_markers;
+}
+
+template <typename T> std::string operators::to_string (T value) {
+    std::ostringstream os;
+    os << value;
+    return os.str ();
 }
