7656e7e2f1174186d414dd73c041ee40adbed62c
Menno van der Graaf <mennovandergraaf@hotmail.com>
2017-05-16 04:34:08 +0200

    fix object fragment shader for GLES

--- resources/GL_shaders/object_fs.glsl
+++ resources/GL_shaders/object_fs.glsl
@@ -9,7 +9,8 @@ out highp vec4 frag_color;
 
 void main()
 {
-    float cosTheta = clamp( dot( vtf_normal, vec3(0, 0, 1) ), 0,1 );
+    float dot_product = dot( vtf_normal, vec3(0, 0, 1) );
+    float cosTheta = clamp(dot_product, 0.0, 1.0);
     vec4 diffuse = vtf_color * cosTheta;
     vec4 ambient = vtf_color * 0.5;
     frag_color = diffuse + ambient;
