2901aac2f42c61040ff3b54b5012c091d98c7633
Menno van der Graaf <mennovandergraaf@hotmail.com>
2017-05-30 03:27:41 +0200

    add force gles option

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,32 @@
 #include <QApplication>
+#include <QCommandLineParser>
 #include <QQmlApplicationEngine>
 #include <QSurfaceFormat>
 
 #include "window.hpp"
 
 int main (int argc, char* argv[]) {
+    QApplication app (argc, argv);
+    QCommandLineParser parser;
+
+    // parse options
+    parser.addHelpOption ();
+    parser.setApplicationDescription (
+    "ARticated: an augmented reality application");
+    QCommandLineOption force_gles_option (
+    "force-gles", "force usage of openGLES");
+    parser.addOption (force_gles_option);
+    parser.process (app);
+    bool force_gles = parser.isSet (force_gles_option);
+
+    // set GL version
     QSurfaceFormat glFormat;
-    if (QOpenGLContext::openGLModuleType () == QOpenGLContext::LibGL) {
-        // on desktop, require opengl 4.3
-        glFormat.setVersion (4, 3);
+    if (QOpenGLContext::openGLModuleType () == QOpenGLContext::LibGL && !force_gles) {
+        // on desktop, require opengl 4.1
+        glFormat.setVersion (4, 1);
     } else {
         // on mobile, require opengles 3.0
+        glFormat.setRenderableType (QSurfaceFormat::OpenGLES);
         glFormat.setVersion (3, 0);
     }
 
@@ -18,7 +34,6 @@ int main (int argc, char* argv[]) {
     QSurfaceFormat::setDefaultFormat (glFormat);
 
     // create the main app window
-    QApplication app (argc, argv);
     Window window;
 
     window.setWindowTitle ("ARticated");
