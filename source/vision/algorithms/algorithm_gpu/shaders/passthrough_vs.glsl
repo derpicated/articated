cefa413ffb3f6d710c12559cfa0867103e1c43b0
Menno van der Graaf <mennovandergraaf@hotmail.com>
2019-09-04 04:29:51 +0200

    Correct Y-axis for vision shader

--- source/vision/algorithms/algorithm_gpu/shaders/passthrough_vs.glsl
+++ source/vision/algorithms/algorithm_gpu/shaders/passthrough_vs.glsl
@@ -8,7 +8,7 @@ out vec2 vtf_texcoord;
 
 void main()
 {
-    gl_Position = vec4(position, 0.0, 1.0);
+    gl_Position = vec4(position.x, -position.y, 0.0, 1.0);
     vtf_texcoord = tex;
     vtf_is_GLRED = is_GLRED;
 }
